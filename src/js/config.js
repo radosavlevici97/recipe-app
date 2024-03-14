4331413bc9e68de72f67a657fa4df77e86d76b1e
radosavlevici97 <radosavlevici.calin1997@gmail.com>
2024-03-14 12:27:36 +0200

    feat: components added

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7bcb2d07-81cb-4269-997c-11e8a7ca0cdc';
+export const MODAL_CLOSE_SEC = 2.5;
